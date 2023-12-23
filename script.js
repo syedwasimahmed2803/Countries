0e0ad8af7bb64242ce940df9699e71c8cef8b358
Syed Wasim Ahmed <syedwasimahmed2803@gmail.com>
2023-12-23 16:36:48 +0530

    Added search button, dropdown, dark-mode and css

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ async function fetchData() {
       div.className = "card";
       section.appendChild(div);
       var flag = document.createElement("img");
+      flag.setAttribute("class", "flag");
       flag.setAttribute("src", element.flags.png);
       div.appendChild(flag);
       var commonName = document.createElement("h3");
@@ -21,13 +22,17 @@ async function fetchData() {
       commonName.textContent = element.name.common;
       div.appendChild(commonName);
       var population = document.createElement("p");
-      population.textContent = `Population: ${element.population}`;
+      population.setAttribute("class", "text");
+      population.innerHTML = `<span>Population:</span> ${element.population.toLocaleString()}`;
       div.appendChild(population);
       var region = document.createElement("p");
-      region.textContent = `Region: ${element.region}`;
+      region.setAttribute("class", "text");
+      region.innerHTML = `<span>Region:</span> ${element.region}`;
       div.appendChild(region);
       var capital = document.createElement("p");
-      capital.textContent = `Capital: ${element.capital}`;
+      capital.setAttribute("class", "text");
+      capital.setAttribute("id", "last-item");
+      capital.innerHTML = `<span>Capital:</span> ${element.capital}`;
       div.appendChild(capital);
       // var commonName = element.name.common;
       // console.log(element.flags.png);
@@ -54,10 +59,13 @@ async function fetchData() {
 }
 
 fetchData();
-
+var toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", toggleMode);
+var nav = document.querySelector("nav");
 var search = document.getElementById("search");
 search.addEventListener("keyup", searchItems);
 var section = document.querySelector("section");
+var text = document.getElementById("mode");
 function searchItems(e) {
   var text = e.target.value.toLowerCase();
 
@@ -120,52 +128,78 @@ function createCard(element) {
   var section = document.querySelector(".section");
   var div = document.createElement("div");
   div.className = "card";
+  if (text.textContent != "Dark Mode") {
+    div.classList.add("not-as-dark");
+  } else {
+    div.classList.remove("not-as-dark");
+  }
   section.appendChild(div);
   var flag = document.createElement("img");
+  flag.setAttribute("class", "flag");
   flag.setAttribute("src", element.flags.png);
   div.appendChild(flag);
   var commonName = document.createElement("h3");
+  commonName.setAttribute("id", "name");
   commonName.textContent = element.name.common;
   div.appendChild(commonName);
   var population = document.createElement("p");
-  population.textContent = `Population: ${element.population}`;
+  population.setAttribute("class", "text");
+  population.innerHTML = `<span>Population:</span> ${element.population.toLocaleString()}`;
   div.appendChild(population);
   var region = document.createElement("p");
-  region.textContent = `Region: ${element.region}`;
+  region.setAttribute("class", "text");
+  region.innerHTML = `<span>Region:</span> ${element.region}`;
   div.appendChild(region);
   var capital = document.createElement("p");
-  capital.textContent = `Capital: ${element.capital}`;
+  capital.setAttribute("class", "text");
+  capital.setAttribute("id", "last-item");
+  capital.innerHTML = `<span>Capital:</span> ${element.capital}`;
   div.appendChild(capital);
 }
 
-var toggle = document.querySelector(".toggle");
-toggle.addEventListener("click", toggleMode);
-var nav = document.querySelector("nav");
 function toggleMode(e) {
-  var text = document.getElementById("mode");
   var input = document.getElementById("search");
   var header = document.getElementsByTagName("header")[0];
   var dropdown = document.getElementsByClassName("dropbtn")[0];
   var body = document.getElementsByTagName("body")[0];
-  var content = document.querySelectorAll("a:hover");
+  var search = document.querySelector(".search-container");
+  var searchIcon = document.querySelector(".fa-magnifying-glass");
+  var content = document.querySelectorAll(".dropdown-content a");
+  var card = document.querySelectorAll(".card");
+  var icon = document.querySelector(".fa-regular");
   if (text.textContent == "Dark Mode") {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
     text.textContent = "Light Mode";
     section.classList.add("dark");
     body.classList.add("dark");
     nav.classList.add("not-as-dark");
     input.classList.add("not-as-dark");
     dropdown.classList.add("not-as-dark");
+    card.forEach((item) => item.classList.add("not-as-dark"));
+    search.classList.add("not-as-dark");
+    searchIcon.classList.add("not-as-dark");
+    // searchIcon.style.backgroundColor = "red";
+    // searchIcon.style.color = "hsl(0, 0%, 52%)";
 
+    // dropdownContent.classList.add("not-as-dark");
+    // content.classList.add("not-as-dark");
     header.classList.add("dark");
-    content.classList.add("not-as-dark");
+    content.forEach((item) => item.classList.add("not-as-dark"));
   } else {
     text.textContent = "Dark Mode";
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
     section.classList.remove("dark");
     body.classList.remove("dark");
     nav.classList.remove("not-as-dark");
     input.classList.remove("not-as-dark");
     header.classList.remove("dark");
     dropdown.classList.remove("not-as-dark");
-    content.classList.remove("not-as-dark");
+    // dropdownContent.classList.remove("not-as-dark");
+    search.classList.remove("not-as-dark");
+    searchIcon.classList.remove("not-as-dark");
+    content.forEach((item) => item.classList.remove("not-as-dark"));
+    card.forEach((item) => item.classList.remove("not-as-dark"));
   }
 }
